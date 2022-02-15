d5e08bd4a84f35d9b5a7326bd881daf8dee805ee
Lukas Nies <Lukas.Nies@cern.ch>
2022-02-15 20:42:17 +0100

    Removed plotting from utilities
    Removed plotting from utilities and transfered them to plot module

--- bin/utilities.py
+++ bin/utilities.py
@@ -32,134 +32,6 @@ from process import ProcessorBase, MPANTMpa
 
 FILE_LOCATION = os.path.dirname(__file__)+"/"
 
-def custom_colors():
-	# Color definition from coolors.co
-	colors = {
-		# https://coolors.co/d1495b-edae49-abe188-1a6fdf-0c0a3e
-		'Fall_rgb': {'red':'#D1495B', 'orange':'#EDAE49', 'green':'#ABE188','blue':'#1A6FDF','purple':'#0C0A3E'},
-		'Jo-s_favs': 
-		{				
-						'black': "#000000",
-						'red': "#FF2D55", 
-						'blue': "#00A2FF",
-						'orange': "#FFCC00", 
-						'green': "#61D935", 
-						'grey': "#C0C0C0", 
-						'purple': "#C177DA", 
-						'lightblue': "#6FF1E9",
-		}
-	}
-	return colors
-
-def simple_error_plt(y, y_err, x='', x_labels='', \
-					 label = ["ISOLTRAP"], x_label='', y_label=[''], title='', \
-					 ref_value=None, ref_err=None, ref_legend_label='AME20 Error', ref_axis=0,
-					 x_share = False,
-					 ):
-	'''
-	Simple scatter plot with y error bars.
-	Parameters:
-	- y: y-values
-	- y_err: y-errors
-	- x: x-data (exclusive with x_labels)
-	- x_labels: array of strings to be used as x-labels
-	- x_label: x-axis labeling
-	- y_label: y-axis labeling
-	- title: plot title
-	'''
-	colors = custom_colors()
-	colors_keys = list(colors['Jo-s_favs'].keys())
-	mpl.rc('text', usetex=False)
-	fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(4.5*1.5, 4.5))
-	
-	if len(x_labels) != 0 and len(x) == 0:
-		x = np.arange(0,len(x_labels),1)
-		plt.xticks(x, x_labels, size = 10, rotation = 0)
-	elif len(x_labels) == '' and len(x) != '':
-		x = x
-	else:
-		print("Wrong x-data passed. Can only pass either x or x_labels")
-
-	# Loop through list of arrays passed
-	twins = []
-	for i in np.arange(0,len(y),1):
-		# If x_labels are passed
-		if len(x_labels) != 0:
-			x_plot = np.arange(0,len(x_labels),1)
-		else:
-			x_plot = x[i]
-		#
-		if i == 0:
-			ax.errorbar(x_plot, y[i], y_err[i],
-					   fmt='o', color=colors['Jo-s_favs'][colors_keys[i]], zorder = 2, 
-					   label=label[i], 
-					   fillstyle='full', mfc="black", linewidth=2, ms =10
-			)
-			ax.set_ylabel(y_label[i], size=18) #fontweight='bold')
-			ax.tick_params(direction='out')
-		else:
-			if x_share:
-				twins.append(ax.twinx())
-				twins[i-1].spines['right'].set_position(("axes", 0.8+i*0.2))
-				color = colors['Jo-s_favs'][colors_keys[i]]
-				twins[i-1].set_ylabel(y_label[i], color=color, fontsize=18)  # we already handled the x-label with ax1
-				twins[i-1].tick_params(axis = 'y', direction='in', color=color)
-				twins[i-1].yaxis.label.set_color(color)
-				ax.spines["right"].set_color(color)
-				twins[i-1].spines["right"].set_color(color)
-			else:
-				twins.append(ax)
-				color = colors['Jo-s_favs'][colors_keys[i]]
-
-			twins[i-1].errorbar(x_plot, y[i], y_err[i],
-				   fmt='o', color=color, zorder = 2, 
-				   label=label[i], 
-				   fillstyle='full', mfc=color, linewidth=2, ms =10
-			)
-
-			ax.set_zorder(twins[i-1].get_zorder()+1)
-			ax.patch.set_visible(False)
-
-
-	#
-
-	# ax_t = ax.secondary_xaxis('top')
-	# ax_t.set_xticks(x)
-
-	ax.set_xlabel(x_label, size=18) #fontweight='bold')
-	# ax_r = ax.secondary_yaxis('right')
-	# # ax.set_xlabel("Mercury Isotopes", size=14, fontweight='bold')
-	# ax_t.tick_params(axis='x', direction='in', labeltop=False)
-	# ax_r.tick_params(axis='y', direction='in', labelright=False)
-
-
-	if ref_value is not None and ref_err is not None:
-		# Error band
-		if len(x_labels) != 0:
-			# 
-			x_plot = np.arange(0,len(x_labels),1)
-			if ref_axis == 0:
-				ax.fill_between(x_plot, ref_value-ref_err, ref_value+ref_err, facecolor='0.5', alpha=0.5,
-						label = ref_legend_label)
-				ax.plot(x_plot, [ref_value for value in x_plot], color='grey')
-			else:
-				twins[ref_axis-1].fill_between(x_plot, ref_value-ref_err, ref_value+ref_err, facecolor='0.5', alpha=0.5,
-						label = ref_legend_label)
-				twins[ref_axis-1].plot(x_plot, [ref_value for value in x_plot], color='grey')
-
-
-	# plt.axhspan(ref_value-ref_err, ref_value+ref_err, facecolor='0.5', alpha=0.5)
-
-	# handles, labels = ax.get_legend_handles_labels()
-	# handles = [h[0] for h in handles] # Remove errorbars from legend
-	
-	plt.legend()#handles, labels, fontsize=12, frameon=False, ncol=1, loc="upper left")
-	
-	plt.tight_layout()
-	plt.title(title, fontsize=20)
-	# plt.savefig("./Mercury_Masses_Comparison.pdf", dpi=300)
-	plt.show()
-
 def get_time_of_measurement(mpa_file, as_datetime = False):
 	"""
 	Fetches time of measurement from data file 
@@ -701,7 +573,7 @@ class MRToFIsotope(MRToFUtils):
 						t_isotope='', t_ref1='', t_ref2='',
 						t_isotope_err='', t_ref1_err='', t_ref2_err='',
 						centroid = 'mu0', tweak_tofs = [0,0,0],
-						is_doublet = False,
+						is_doublet = False, dt = '', dt_err = '',
 						):
 		"""
 		Stores information passed to calc_mass or calc_exc_energy and loads fit files if passed
@@ -717,8 +589,8 @@ class MRToFIsotope(MRToFUtils):
 			self.isotope_gs_t_err = float(self.isotope_fit.get_val('mu0', 'error'))
 			# If isomer is passed, store isomere ToF or ToF difference as well
 			if self.state != 'gs':
-				# If doublet is passed
-				if is_doublet:
+				# If doublet is passed as fit file
+				if is_doublet and dt == '':
 					self.isotope_m_dt = float(self.isotope_fit.get_val(self.doublet_key, 'value')) + tweak_tofs[0]
 					self.isotope_m_dt_err = float(self.isotope_fit.get_val(self.doublet_key, 'error'))
 				else:
@@ -730,12 +602,12 @@ class MRToFIsotope(MRToFUtils):
 			self.isotope_gs_t_err = t_isotope_err
 			# If isomere ToF is passed, set gs to -1 and store isomere ToF instead 
 			if self.state != 'gs':
-				self.isotope_gs_t = -1 
-				self.isotope_gs_t_err = -1 
+				# self.isotope_gs_t = -1 
+				# self.isotope_gs_t_err = -1 
 				# If passed as doublet
 				if is_doublet:
-					self.isotope_m_dt = dt_isotope + tweak_tofs[0]
-					self.isotope_m_dt_err = dt_isotope_err
+					self.isotope_m_dt =  t_ref1 - self.isotope_gs_t
+					self.isotope_m_dt_err = math.sqrt(t_ref1_err**2 + self.isotope_gs_t_err**2)
 				else:
 					self.isotope_m_t = t_isotope + tweak_tofs[0]
 					self.isotope_m_t_err = t_isotope_err
